708e50c882a4775382fad6ff3a98d8edfa76bebb
Shahriar Islam Santo <santo.ictbaust25@gmail.com>
2026-08-01 15:03:06 +0600

    Implement while loop to print numbers and calculate sum
    Fix loop condition and increment logic in while loop.

---
+++ C Codes/C Codes/C Codes/C Codes/C Codes/C Codes/C Codes/C Codes/C Codes/C Codes/C Codes/C Codes/write_a_loop_while.c
@@ -0,0 +1,22 @@
+/*
+Developed By : Md. Shahriar Islam Santo
+               Dept. of ICT, BAUST
+               Date: 18/09/2025
+*/
+
+#include <stdio.h>
+
+int main (){
+    int i, sum;
+ i=4,sum=4;
+
+ while (i<200); {
+    printf ("%d+",i);
+    sum=sum+1;
+    i+=4;}
+
+ printf ("\n Sum =%d", sum);
+
+ return  0;
+}
+
